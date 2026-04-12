cdfd6d9cdce1eb75ecc3e5d2e63010b678c92fbe
Taha Rashid <taharashid710192@gmail.com>
2026-04-12 12:07:07 -0400

    added proper mp3 playback

--- source/main.cpp
+++ source/main.cpp
@@ -43,7 +43,7 @@ int main(void) {
     // SwitchView(new IntroView());
 
     // DEBUG
-    SwitchView(new IwatodaiDormView());
+    SwitchView(new MusicView());
 
 	while(pmMainLoop()) {
 		swiWaitForVBlank();
